b780b84013181fbaa9bc88df126a61ef12886aeb
marcello-s <marcello.s@gmx.ch>
2025-09-30 19:53:07 +0200

    link to json library and add a demo (#3)

--- src/helloworld.cpp
+++ src/helloworld.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <nlohmann/json.hpp>
 
 using namespace std;
+using json = nlohmann::json;
 
 int main()
 {
@@ -13,4 +15,13 @@ int main()
         cout << word << " ";
     }
     cout << endl;
+	
+	// read file
+    auto json = json::parse("{\"entry1\": \"string\"}");
+
+    // mutate the json
+    json["entry1"] = "kilroy was here!";
+
+    // write to a stream, or the same file
+    std::cout << json; // print the json
 }
